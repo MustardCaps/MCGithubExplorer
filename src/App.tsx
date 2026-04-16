2db77ea7fcb31b0e5a2b0a16bb47b2cd0e6ba3c1
MustardCaps <joe@paulsenweb.com>
2026-04-15 19:33:52 -0700

    Add input and other shadcn components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Input from '@components/ui/Input';
+
 function App() {
 	return (
 		<div className="min-h-screen bg-background text-foreground p-8">
 			<h1 className="text-3xl font-bold">MCGithubExplorer</h1>
+			<div className='max-w-2xl mx-auto'>
+				<Input />
+			</div>
 		</div>
 	);
 }
